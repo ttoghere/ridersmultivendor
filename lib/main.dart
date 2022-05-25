574970f8267e39cd97b0c6367f9c5eb403c68e4d
ttoghere <59641232+ttoghere@users.noreply.github.com>
2022-05-25 13:36:36 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ridersmultivendor/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+//
 import 'controllers/global.dart';
 import 'view/splashScreen/splash_screen.dart';
 
